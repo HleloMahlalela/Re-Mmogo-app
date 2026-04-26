560b777b334e0fa59f31eeaf67a49371f06217d8
HleloMahlalela <mh24020091@biust.ac.bw>
2026-04-26 21:57:45 +0200

    Navbar and Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,12 +3,12 @@ import "../styles/register.css";
 function Register() {
   return (
     <div className="register-page">
-      {/* Main card */}
+      {/*main card*/}
       <div className="register-card">
         <h1>Create Account</h1>
         <p>Join Re-Mmogo and manage your Motshelo group easily.</p>
 
-        {/* Registration form */}
+        {/*registration form*/}
         <form className="register-form">
           {/* Full Name */}
           <input
@@ -16,37 +16,37 @@ function Register() {
             placeholder="Full Name"
           />
 
-          {/* Email */}
+          {/*email*/}
           <input
             type="email"
             placeholder="Email Address"
           />
 
-          {/* Phone */}
+          {/*contact*/}
           <input
             type="text"
             placeholder="Phone Number"
           />
 
-          {/* Password */}
+          {/*password*/}
           <input
             type="password"
             placeholder="Password"
           />
 
-          {/* Confirm Password */}
+          {/*confirm password*/}
           <input
             type="password"
             placeholder="Confirm Password"
           />
 
-          {/* Submit Button */}
+          {/*submit button*/}
           <button type="submit">
             Register
           </button>
         </form>
 
-        {/* Bottom text */}
+        {/*bottom text*/}
         <p className="login-link">
           Already have an account? <a href="#">Login</a>
         </p>
